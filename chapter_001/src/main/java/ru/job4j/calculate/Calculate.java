4d00496d966ddeaa471a965fb60f798521a52a68
MikhailSamorodov <mailto:samorodoff.mix@yandex.ru>
2018-09-04 11:40:49 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -15,4 +15,17 @@ public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+ /**
+     * Method echo.
+     * @param name SamorodovMA.
+     * @return Echo plus SamorodovMA.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
+
+
+
+
